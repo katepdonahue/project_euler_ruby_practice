1878a1de0391eefcfc1ba067534fd87777f3d36f
Kate <katepdonahue@gmail.com>
2014-03-17 11:09:25 -0400

    Write a couple method objectives for test

--- multiples_3_5/spec.rb
+++ multiples_3_5/spec.rb
@@ -2,8 +2,16 @@ require './multiples.rb'
 
 describe Multiple do
 
+  describe 'mults' do
+    it "should find all the multiples of 3 and 5 below 1000" do
+
+    end
+  end
+
   describe 'sumult' do
-    
+    it "should sum all the multiples of 3 and 5 below 1000" do
+
+    end
   end
 
 end
