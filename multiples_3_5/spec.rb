e2133a026b891a6c16d8225f28dced367b1c0fe7
Kate <katepdonahue@gmail.com>
2014-03-17 11:13:39 -0400

    Complete mults test, realize should be testing for known situation: change 1000 to 10

--- multiples_3_5/spec.rb
+++ multiples_3_5/spec.rb
@@ -2,14 +2,14 @@ require './multiples.rb'
 
 describe Multiple do
 
-  describe 'mults' do
-    it "should find all the multiples of 3 and 5 below 1000" do
-
+  describe '#mults' do
+    it "should find all the multiples of 3 and 5 below 10" do
+      expect(Multiple.new(3, 5, 10).mults).to eq([3, 5, 6, 9])
     end
   end
 
-  describe 'sumult' do
-    it "should sum all the multiples of 3 and 5 below 1000" do
+  describe '#sumult' do
+    it "should sum all the multiples of 3 and 5 below 10" do
 
     end
   end
