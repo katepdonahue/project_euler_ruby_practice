be3da995b529299c06cc57dc31193715be3f1b00
Kate <katepdonahue@gmail.com>
2014-03-18 10:04:42 -0400

    Write test for sum_primes

--- consec_primes/spec.rb
+++ consec_primes/spec.rb
@@ -1,21 +1,23 @@
 require "./consec_primes"
 
 describe ConsecPrime do
-  cutoff = ConsecPrime.new(15)
   
   describe "#find_primes" do
+    cutoff = ConsecPrime.new(15)
     it "it should find the primes below your number" do
       expect(cutoff.find_primes).to eq([2, 3, 5, 7, 11, 13])
     end
   end
 
   describe "#sum_primes" do
+    cutoff = ConsecPrime.new(100)  
     it "should sum the consecutive primes until it reaches your number, return the previous result" do
-
+      expect(cutoff.sum_primes).to eq(41)
     end
   end
 
   describe "#is_prime?" do
+    cutoff = ConsecPrime.new(15)
     it "should return true if a given number is a prime" do
       expect(cutoff.is_prime?(3)).to eq(true)
     end
