3643df6d865d23fdbc4e0f71d71796426b570856
Kate <katepdonahue@gmail.com>
2014-03-24 12:47:42 -0400

    Write tests for array reverse

---
+++ reverse_array/spec.rb
@@ -0,0 +1,22 @@
+require './reverse_array'
+
+# Write a function to reverse an array of characters in place.
+# "In place" means "without creating a new string in memory."
+
+describe Array, '#my_reverse' do
+  
+  it "should reverse an array of even elements" do
+    expect([0, 3, nil, 6].my_reverse).to eq([6, nil, 3, 0])
+  end
+  it "should reverse an array of odd elements" do
+    expect([0, 3, 5].my_reverse).to eq([5, 3, 0])
+  end
+  it "should not break when array has only one element" do
+    expect([0].my_reverse).to eq([0])
+  end
+  it "should return nil when the array is nil" do
+    my_array = nil
+    expect(my_array.my_reverse).to eq(nil)
+  end
+
+end
