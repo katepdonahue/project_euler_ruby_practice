51434858e8d32cfa926c31135050c96cc0609cc2
Kate <katepdonahue@gmail.com>
2014-03-24 11:34:50 -0400

    Update best_profit method and initialize for getting an array instead of a hash

--- apple_stocks/apple_stocks.rb
+++ apple_stocks/apple_stocks.rb
@@ -1,16 +1,16 @@
 class Stock
   attr_reader :stock_prices_yesterday
 
-  def initialize(stock_hash)
-    @stock_prices_yesterday = stock_hash
+  def initialize(stock_array)
+    @stock_prices_yesterday = stock_array
   end
 
   def best_profit
     # lowest = stock_prices_yesterday["1"]
     # first, the slowest way
     max_diff = 0
-    stock_prices_yesterday.each do |mins1, price1|
-      stock_prices_yesterday.each do |mins2, price2|
+    stock_prices_yesterday.each_with_index do |price1, mins1|
+      stock_prices_yesterday.each_with_index do |price2, mins2|
         if mins2 > mins1
           profit = price2 - price1
           if profit > max_diff
