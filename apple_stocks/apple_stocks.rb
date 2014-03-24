8b3ff0ee751d0e8a55165e4bc2d22c4fcf9d9c61
Kate <katepdonahue@gmail.com>
2014-03-24 11:45:17 -0400

    Don't need to check an element as a buy price even if it is the same as a previous low point as long as it's not the first value in the array because of how low_price is set

--- apple_stocks/apple_stocks.rb
+++ apple_stocks/apple_stocks.rb
@@ -9,12 +9,10 @@ class Stock
     low_price = stock_prices_yesterday.first
     max_diff = 0
     stock_prices_yesterday.each_with_index do |price1, mins1|
-      if price1 <= low_price
-        stock_prices_yesterday[(mins1+1)..-1].each do |price2|
+      if price1 < low_price || mins1 == 0
+        stock_prices_yesterday[mins1+1..-1].each do |price2|
           profit = price2 - price1
-          if profit > max_diff
-            max_diff = profit
-          end
+          max_diff = profit if profit > max_diff
         end
         low_price = price1
       end
