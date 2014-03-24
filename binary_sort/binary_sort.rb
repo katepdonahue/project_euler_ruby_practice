b315a1e053e1346f85ed96a93d87f87a1d92e523
Kate <katepdonahue@gmail.com>
2014-03-24 19:55:10 -0400

    Refactor, employing powers of 2

--- binary_sort/binary_sort.rb
+++ binary_sort/binary_sort.rb
@@ -6,37 +6,11 @@ class Array
     sorted_hash = {}
     self.each  do |char|
       sorted_hash[char] = [char.unpack('b*')[0].to_i(2), 0]
-      if sorted_hash[char][0] - 128 >= 0
-        sorted_hash[char][0] -= 128
-        sorted_hash[char][1] += 1
-      end
-      if sorted_hash[char][0] - 64 >= 0
-        sorted_hash[char][0] -= 64
-        sorted_hash[char][1] += 1
-      end
-      if sorted_hash[char][0] - 32 >= 0
-        sorted_hash[char][0] -= 32
-        sorted_hash[char][1] += 1
-      end
-      if sorted_hash[char][0] - 16 >= 0
-        sorted_hash[char][0] -= 16
-        sorted_hash[char][1] += 1
-      end
-      if sorted_hash[char][0] - 8 >= 0
-        sorted_hash[char][0] -= 8
-        sorted_hash[char][1] += 1
-      end
-      if sorted_hash[char][0] - 4 >= 0
-        sorted_hash[char][0] -= 4
-        sorted_hash[char][1] += 1
-      end
-      if sorted_hash[char][0] - 2 >= 0
-        sorted_hash[char][0] -= 2
-        sorted_hash[char][1] += 1
-      end
-      if sorted_hash[char][0] - 1 >= 0
-        sorted_hash[char][0] -= 1
-        sorted_hash[char][1] += 1
+      (0..7).to_a.reverse.each do |i|
+        if sorted_hash[char][0] - 2**i >= 0
+          sorted_hash[char][0] -= 2**i
+          sorted_hash[char][1] += 1
+        end
       end
     end
     sorted_array = []
