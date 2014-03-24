a323b09e7777b316eaad01aa5499d28005192883
Kate <katepdonahue@gmail.com>
2014-03-24 19:22:10 -0400

    Convert binary into base 10 and store it all in a hash

--- binary_sort/binary_sort.rb
+++ binary_sort/binary_sort.rb
@@ -3,9 +3,11 @@ require 'debugger'
 class Array
 
   def binary_sort
-    self.map do |char|
-      char.unpack('b*')[0].to_i
+    sorted_hash = {}
+    self.each  do |char|
+      sorted_hash[char] = char.unpack('b*')[0].to_i(2)
     end
+    sorted_hash
   end
 
 end
