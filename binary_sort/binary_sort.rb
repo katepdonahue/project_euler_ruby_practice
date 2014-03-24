fa99fea9156eed3e34a6224508d97a9c22977de6
Kate <katepdonahue@gmail.com>
2014-03-24 19:36:50 -0400

    Add a place to store the rank to sort it on

--- binary_sort/binary_sort.rb
+++ binary_sort/binary_sort.rb
@@ -5,7 +5,7 @@ class Array
   def binary_sort
     sorted_hash = {}
     self.each  do |char|
-      sorted_hash[char] = char.unpack('b*')[0].to_i(2)
+      sorted_hash[char] = [char.unpack('b*')[0].to_i(2), 0]
     end
     sorted_hash
   end
