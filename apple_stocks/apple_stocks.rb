75a15bcc110506217f21a2cd80c89d21a4e4f385
Kate <katepdonahue@gmail.com>
2014-03-24 11:36:20 -0400

    Increase efficiency by only looking ahead in the array on the second iteration, not at the whole array

--- apple_stocks/apple_stocks.rb
+++ apple_stocks/apple_stocks.rb
@@ -10,12 +10,10 @@ class Stock
     # first, the slowest way
     max_diff = 0
     stock_prices_yesterday.each_with_index do |price1, mins1|
-      stock_prices_yesterday.each_with_index do |price2, mins2|
-        if mins2 > mins1
-          profit = price2 - price1
-          if profit > max_diff
-            max_diff = profit
-          end
+      stock_prices_yesterday[(mins1+1)..-1].each_with_index do |price2, mins2|
+        profit = price2 - price1
+        if profit > max_diff
+          max_diff = profit
         end
       end
     end
