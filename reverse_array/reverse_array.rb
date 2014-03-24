5ea6c9238a348d8f266bc926a507daafd09d1602
Kate <katepdonahue@gmail.com>
2014-03-24 13:05:13 -0400

    Refactor a bit

--- reverse_array/reverse_array.rb
+++ reverse_array/reverse_array.rb
@@ -5,7 +5,7 @@ class Array
   def my_reverse
     last = self.length - 1
     (0..last).each do |i|
-      self[self.length] = self[last-i]
+      self << self[last-i]
     end
     self[last+1..-1]
   end
