5ad2930c24af74b9af63c7f40ff6b3c4306acf0a
Kate <katepdonahue@gmail.com>
2014-03-24 19:05:50 -0400

    Write basic test

--- binary_sort/spec.rb
+++ binary_sort/spec.rb
@@ -1,9 +1,9 @@
-require './ascii_sort'
+require './binary_sort'
 
 # Given an array of characters, sort them by the number 
 # of 1's in the binary version from highest to lowest
 
-describe Array, '#ascii_sort' do
+describe Array, '#binary_sort' do
   
   it "should sort correctly" do
     expect(["a", "c", "o", "e", "z", "A"]).to eq(["A", "a", "c", "e", "z", "o"])
