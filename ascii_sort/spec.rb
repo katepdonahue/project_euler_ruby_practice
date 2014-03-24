47ba7613019d12073ca4c59f039a55a80367044c
Kate <katepdonahue@gmail.com>
2014-03-24 18:50:24 -0400

    Initial commit

---
+++ ascii_sort/spec.rb
@@ -0,0 +1,10 @@
+require './ascii_sort'
+
+# Given an array of characters, sort them by the number 
+# of 1's in the ascii version from highest to lowest
+
+describe Array, '#ascii_sort' do
+  
+
+
+end
