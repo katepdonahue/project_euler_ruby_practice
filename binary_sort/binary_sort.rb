4e0c95060016d283a5a2ddccd01596cd65a199de
Kate <katepdonahue@gmail.com>
2014-03-24 19:15:30 -0400

    Correct test to actually call method and start method by converting characters to ascii

--- binary_sort/binary_sort.rb
+++ binary_sort/binary_sort.rb
@@ -1,4 +1,11 @@
+require 'debugger'
+
 class Array
 
+  def binary_sort
+    self.map do |char|
+      char.unpack('b*')
+    end
+  end
 
 end
--- binary_sort/spec.rb
+++ binary_sort/spec.rb
@@ -1,12 +1,12 @@
 require './binary_sort'
 
 # Given an array of characters, sort them by the number 
-# of 1's in the binary version from highest to lowest
+# of 1's in the ascii 8-bit binary version from highest to lowest
 
 describe Array, '#binary_sort' do
   
   it "should sort correctly" do
-    expect(["a", "c", "o", "e", "z", "A"]).to eq(["A", "a", "c", "e", "z", "o"])
+    expect(["a", "c", "o", "e", "z", "A"].binary_sort).to eq(["A", "a", "c", "e", "z", "o"])
   end
 
 end
