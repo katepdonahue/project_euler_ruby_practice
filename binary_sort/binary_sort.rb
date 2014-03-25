6bebce9429563a677c9928730728ab96c702a635
Kate <katepdonahue@gmail.com>
2014-03-24 20:15:34 -0400

    Refactor binary_sort method using binary_ones string method wow

--- binary_sort/binary_sort.rb
+++ binary_sort/binary_sort.rb
@@ -3,21 +3,7 @@ require 'debugger'
 class Array
 
   def binary_sort
-    sorted_hash = {}
-    self.each  do |char|
-      sorted_hash[char] = [char.unpack('b*')[0].to_i(2), 0]
-      (0..7).to_a.reverse.each do |i|
-        if sorted_hash[char][0] - 2**i >= 0
-          sorted_hash[char][0] -= 2**i
-          sorted_hash[char][1] += 1
-        end
-      end
-    end
-    sorted_array = []
-    sorted_hash.sort_by{|char, array| array[1]}.reverse.each do |nest_array|
-      sorted_array << nest_array[0]
-    end
-    sorted_array
+    self.sort_by{|char| char.binary_ones}.reverse
   end
 
 end
